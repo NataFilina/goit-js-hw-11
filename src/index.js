0bddac2d14298ec7fd6337193f1385352a69c35f
NataFilina <filinanatash@gmail.com>
2023-10-27 00:56:23 +0300

    serch and create murkup

--- src/index.js
+++ src/index.js
@@ -3,34 +3,70 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-axios.defaults.headers.common["key"] = "40263466-7e415f5d97d0e5ac2b44404cf";
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-// function fetchBreeds() {  
-//     return axios.get('/breeds')
-//         .then(function (response) {
-//             if (response.status >= 400) {
-//                 throw new Error (response.statusText || "Упс! Щось пішло не так")
-//             }
-//             return response;
-//         })
-        
-// }
+const elements = {
+    form: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+}
 
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-    // <p class="info-item">
-    //   <b>Likes</b>
-    // </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>
+elements.form.addEventListener("submit", onSearch);
+
+async function onSearch(event) {
+  event.preventDefault();
+
+    const photos = event.target.searchQuery.value;
+
+  try {
+      const getPhotos = await searchPhotos(photos);
+      console.log(getPhotos);
+
+    elements.gallery.insertAdjacentHTML("afterbegin", await createMarkup(getPhotos));
+  } catch (err) {
+    console.log(err);
+  } finally {
+    elements.form.reset();
+  }
+}
+
+async function searchPhotos(photos) {
+    const params = new URLSearchParams({
+        key: "40263466-7e415f5d97d0e5ac2b44404cf",
+        q: photos,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true
+    });
+    const resp = await axios.get(`?${params}`)
+        .then(function (response) {
+            if (response.status >= 400) {
+                throw new Error(response.statusText || "Упс! Щось пішло не так")
+            }
+            return response;
+        });
+    return resp
+}
+
+function createMarkup(arr) {
+    return arr.data.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+        <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes</b> ${likes}
+                </p>
+                <p class="info-item">
+                <b>Views</b> ${views}
+                </p>
+                <p class="info-item">
+                <b>Comments</b> ${comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads</b> ${downloads}
+                </p>
+            </div>
+        </div>
+        `
+    });
+}
